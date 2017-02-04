6b142d0b1662b9860c461c28bf573034c53fea80
Kevin Wilson <kevinjacobwilson@gmail.com>
2017-02-04 13:25:42 -0600

    Finalized with some comments

--- invites.js
+++ invites.js
@@ -2,182 +2,294 @@ $(document).ready(function(){
   //make references
   var iuRef = firebase.database().ref().child("invitedUsers");
   //init storage
-  var invitations = {};
+  var invitations = {}; // stores all inviters and # of invites
   var user;
   var invBy;
-  var reversed = {};
-  var chains = {};
+  var reversed = {}; // stores list of invitees for each inviter
+  var chains = {}; // stores invite chain data for inviters
   var invites = 0;
+
+  //track max inviter and # invites
   var maxInvites = 0;
   var maxInviter = "nobody";
 
   var payInviters = {};
 
+  //iterate through invited users to find all invite chains
   iuRef.orderByChild("invitedBy").once("value", function(snapshot){ //snapshot.key = invitedUsers
     snapshot.forEach(function(cSnapshot){ // cSnapshot.key = invitee
       user = cSnapshot.key; //invitee
       invBy = cSnapshot.val().invitedBy; //inviter
+
       if(user != invBy){ //not self invited
         invites++;
       }
+
       if(invitations.hasOwnProperty(invBy)){
-        var temp = invitations[invBy]++;
-        if( temp > maxInvites ){
+        var temp = invitations[invBy]++;//increment num invites for this user if in list
+        if( temp > maxInvites ){ //check max values
           maxInvites = temp;
           maxInviter = invBy;
         }
       }
       else{
-        invitations[invBy] = 1;
+        invitations[invBy] = 1; //set value if not in list yet
       }
 
-      if(!reversed.hasOwnProperty(invBy)){
+      if(!reversed.hasOwnProperty(invBy)){//populate reversed and chains objects with inviters
         reversed[invBy] = {};//holds invites from an inviter
         chains[invBy] = {};//holds chain stats from inviter
       }
-      reversed[invBy][user] = invitations[invBy];
 
+      reversed[invBy][user] = invitations[invBy];//stores invitee with their number in order (first invitee, etc.)
     });
 
-    //use this to count total invites, mostly for debugging
+    //get activePayments
+    var actPayments = {};//to store number of active payments for each user
+    firebase.database().ref().child("activePayments").orderByKey().once("value", function(apSnap){
+      apSnap.forEach(function(ap){//ap.key is an active payment
 
-    //var total = 0;
-    //var count = 0;
-    /*if(!snapshot.child(key).exists()){//OG inviter, not on invited list
-      total += chains[key]["avg"]*chains[key]["cns"];
-      count += countChildren(reversed, key);
-    }*/
+        var sid = ap.val()["sender_id"];
+        var rid = ap.val()["recip_id"];
 
-    var ovMax = 0; //overall max chain length
-    var ovMaxID = ""; //uID of og inviter in max chain
+        actPayments[sid] = (actPayments.hasOwnProperty(sid) ? actPayments[sid] + 1 : 1);//increment if sender
+        actPayments[rid] = (actPayments.hasOwnProperty(rid) ? actPayments[rid] + 1 : 1);//increment if recip
 
-    var ovAvg = 0; //overall average chain length
-    var chaintot = 0; //total number of chains
-
-    var ovCns = 0; //overall max number of chains by user
-    var ovCnsID = ""; //user with max number of chains
-
-    for(key in reversed){ //for each inviter
-      if(!chains[key].hasOwnProperty("max")){//if no data,
-        chains[key] = chainData(reversed,key,chains);// add data
-      }
-
-      if(!snapshot.child(key).exists()){//not in snap = OG inviter
-        if(chains[key]["max"] > ovMax){
-          ovMax = chains[key]["max"];
-          ovMaxID = key;
-        }
-        if(chains[key]["cns"] > ovCns){
-          ovCns = chains[key]["cns"];
-          ovCnsID = key;
-        }
-        ovAvg += chains[key]["avg"]*chains[key]["cns"];
-        chaintot += chains[key]["cns"];
-      }
-    }
-
-    //make FlexList for side panel
-    var invFlexList = '<div class="flex-list" id="purposeList">Top Inviters</div>';
-    var count = 0;
-    var array = [];
-    for(key in reversed){
-      var numInv = 0;
-      for(child in reversed[key]){
-        numInv++;
-      }
-      array.push([key,numInv]);
-    }
-    array.sort(function(a,b){return b[1]-a[1]}); //sort by most invites sent
-
-    for(var i = 0; i < array.length; i++){
-      invFlexList += '<div class="flex-list" id="' + array[i][0] + '">' + (++count) + '. ' + array[i][0] + ': ' + array[i][1] + '</div>';
-      if(count > 20){
-        break;
-      }
-    }
-    while(count < 20){
-      count++;
-      invFlexList += '<div class="flex-list" id="empty"></div>';
-    }
-    invFlexList += '<div class="flex-list" id="tail" style="background:rgba(5,5,5,0);font-size:0px;margin-top:0px;padding-top:0px"><font color = orange>.</div>';
-
-    var chainFlexList = '<div class="flex-list" id="purposeList">Largest Invite Chains</div>';
-    var count = 0;
-    var array = [];
-    for(key in chains){
-      array.push([key,chains[key]["max"]]);
-    }
-    array.sort(function(a,b){return b[1]-a[1]}); //sort by most invites sent
-
-    for(var i = 0; i < array.length; i++){
-      chainFlexList += '<div class="flex-list" id="' + array[i][0] + '">' + (++count) + '. ' + array[i][0] + ': ' + array[i][1] + '&nbsp&nbsp&nbsp Avg: ' + Math.round(chains[array[i][0]]["avg"]*100)/100 + '</div>';
-      if(count >= 20){
-        break;
-      }
-    }
-    while(count < 20){
-      count++;
-      chainFlexList += '<div class="flex-list" id="empty"></div>';
-    }
-    chainFlexList += '<div class="flex-list" id="tail" style="background:rgba(5,5,5,0);font-size:0px;margin-top:0px;padding-top:0px"><font color = orange>.</div>';
-
-
-    document.getElementById("chainLength").innerHTML = "<br/><br/>Max chain length: " + ovMax + "<br/>By User<br/><font size = 3>" + (ovMaxID.length > 20 ? ovMaxID.substr(0,19)+"...":ovMaxID) + "</font><br/><br/>Average chain length: " + (chaintot = 0 ? 0 : Math.round(ovAvg/chaintot*100)/100);
-    document.getElementById("inviteStats").innerHTML = "<br/><br/>Most invites sent: " + maxInvites + "<br/>By User<br/><font size = 3>" + (maxInviter.length > 20 ? maxInviter.substr(0,19)+"...":maxInviter) + "</font><br/><br/>Most chains: " + ovCns + "<br/>By User<br/><font size = 3>" + (ovCnsID.length > 20 ? ovCnsID.substr(0,19)+"...":ovCnsID);
-    document.getElementById("chainLength").onclick = function(){document.getElementById("display").innerHTML = chainFlexList};
-    document.getElementById("inviteStats").onclick = function(){document.getElementById("display").innerHTML = invFlexList};
-
-
-
-    //Do interesting bonus stuff
-    var purpList = {};
-    var maxPurpNum = 0;
-    var maxPurpose = "";
-    var piRef = firebase.database().ref().child("paymentInvites");
-    piRef.once("value", function(piSnap){
-      piSnap.forEach(function(piChild){
-        var inviter = piChild.val().inviter;
-        var invitee = (piChild.val().invitee == "sender" ? piChild.val().sender_name : piChild.val().recip_name);
-        var purpose = piChild.val().purpose;
-        if(!payInviters.hasOwnProperty(inviter)){
-          payInviters[inviter] = {};
-        }
-        purpList[purpose] = (purpList.hasOwnProperty(purpose) ? purpList[purpose] + 1 : 1);
       });
-      //do stuff with purpList after the snap is generated
-      for(key in purpList){
-        if( purpList[key] > maxPurpNum ){
-          maxPurpNum = purpList[key];
-          maxPurpose = key;
+
+      //use this to count total invites, mostly for debugging
+
+      //var total = 0;
+      //var count = 0;
+      /*if(!snapshot.child(key).exists()){//OG inviter, not on invited list
+        total += chains[key]["avg"]*chains[key]["cns"];
+        count += countChildren(reversed, key);
+      }*/
+
+      var ovMax = 0; //overall max chain length
+      var ovMaxID = ""; //uID of og inviter in max chain
+
+      var ovAvg = 0; //overall average chain length
+      var chaintot = 0; //total number of chains
+
+      var ovCns = 0; //overall max number of chains by user
+      var ovCnsID = ""; //user with max number of chains
+
+      for(key in reversed){ //for each inviter
+        if(!chains[key].hasOwnProperty("max")){//if no data,
+          chains[key] = chainData(reversed,key,chains);// add data
+        }
+
+        if(!snapshot.child(key).exists()){//not in snap = OG inviter
+          //update max values (OGs should have larges)
+          if(chains[key]["max"] > ovMax){
+            ovMax = chains[key]["max"];
+            ovMaxID = key;
+          }
+          if(chains[key]["cns"] > ovCns){
+            ovCns = chains[key]["cns"];
+            ovCnsID = key;
+          }
+          ovAvg += chains[key]["avg"]*chains[key]["cns"];//calculate overall avg
+          chaintot += chains[key]["cns"];//calculate overall number of chains
         }
       }
 
-      var purpFlexList = '<div class="flex-list" id="purposeList">Top Purposes for Payment Invites</div>';
+      //make FlexList for invite data
+      var invFlexList = '<div class="flex-list" id="purposeList">Top Inviters</div>';
       var count = 0;
       var array = [];
-      for(key in purpList){
-        array.push([key,purpList[key]]);
+      for(key in reversed){
+        var numInv = 0;
+        for(child in reversed[key]){
+          numInv++;
+        }
+        array.push([key,numInv]);
+      }
+      array.sort(function(a,b){return b[1]-a[1]}); //sort by most invites sent
+      for(var i = 0; i < array.length; i++){
+        var ari = array[i];
+        invFlexList += '<div class="flex-list" id="' + ari[0] + '">' + (++count) + '. ' + ari[0] + ': ' + ari[1] + ' &nbsp AP: ' + (actPayments.hasOwnProperty(ari[0]) ? actPayments[ari[0]] : 0) + '</div>';
+        if(count > 20){
+          break;
+        }
+      }
+      while(count < 20){//fill List to appropriate size
+        count++;
+        invFlexList += '<div class="flex-list" id="empty"></div>';
+      }
+      //for appearance of list only
+      invFlexList += '<div class="flex-list" id="tail" style="background:rgba(5,5,5,0);font-size:0px;margin-top:0px;padding-top:0px"><font color = orange>.</div>';
+
+
+
+      //create FlexList for chain data
+      var chainFlexList = '<div class="flex-list" id="purposeList">Largest Invite Chains</div>';
+      var count = 0;
+      var array = []; //for sorting
+      for(key in chains){
+        array.push([key,chains[key]["max"]]);
       }
       array.sort(function(a,b){return b[1]-a[1]}); //sort by most invites sent
 
       for(var i = 0; i < array.length; i++){
-        purpFlexList += '<div class="flex-list" id="' + array[i][0] + '">' + (++count) + '. ' + array[i][0] + ': ' + array[i][1] + '</div>';
+        var ari = array[i];
+        chainFlexList += '<div class="flex-list" id="' + ari[0] + '">' + (++count) + '. ' + ari[0] + ': ' + ari[1] + '&nbsp&nbsp&nbsp Avg: ' + Math.round(chains[ari[0]]["avg"]*100)/100 + '</div>';
         if(count >= 20){
           break;
         }
       }
       while(count < 20){
         count++;
-        purpFlexList += '<div class="flex-list" id="empty"></div>';
+        chainFlexList += '<div class="flex-list" id="empty"></div>';
       }
-      purpFlexList += '<div class="flex-list" id="tail" style="background:rgba(5,5,5,0);font-size:0px;margin-top:0px;padding-top:0px"><font color = orange>.</div>';
+      chainFlexList += '<div class="flex-list" id="tail" style="background:rgba(5,5,5,0);font-size:0px;margin-top:0px;padding-top:0px"><font color = orange>.</div>';
 
 
-      document.getElementById("invPurpose").innerHTML = "<br/><br/>Top PayInvite Purpose:<br/>" + maxPurpose + "<br/><br/>Total: <font size = 3>" + maxPurpNum;
-      document.getElementById("invPurpose").onclick = function(){document.getElementById("display").innerHTML = purpFlexList};
+      document.getElementById("chainLength").innerHTML = "<br/><br/>Max chain length: " + ovMax + "<br/>By User<br/>" + (ovMaxID.length > 17 ? ovMaxID.substr(0,16)+"...":ovMaxID) + "</font><br/><br/>Average chain length: " + (chaintot = 0 ? 0 : Math.round(ovAvg/chaintot*100)/100);
+      document.getElementById("inviteStats").innerHTML = "<br/><br/>Most invites sent: " + maxInvites + "<br/>By User<br/>" + (maxInviter.length > 17 ? maxInviter.substr(0,16)+"...":maxInviter) + "</font><br/><br/>Most chains: " + ovCns + "<br/>By User<br/>" + (ovCnsID.length > 17 ? ovCnsID.substr(0,16)+"...":ovCnsID);
+      document.getElementById("chainLength").onclick = function(){document.getElementById("display").innerHTML = chainFlexList};
+      document.getElementById("inviteStats").onclick = function(){document.getElementById("display").innerHTML = invFlexList};
+
+
+      //get purposes from each paymentInvite (can use for any payments with new ref)
+
+      var purpList = {};
+      var maxPurpNum = 0;
+      var sender = 0;
+      var recip = 0;
+      var maxPurpose = "";
+
+      var piRef = firebase.database().ref().child("paymentInvites");
+      piRef.once("value", function(piSnap){
+        piSnap.forEach(function(piChild){
+          if(piChild.val().invitee == "sender"){ sender++ }
+          else{ recip++ }
+
+          var inviter = piChild.val().inviter;
+          var invitee = (piChild.val().invitee == "sender" ? piChild.val().sender_name : piChild.val().recip_name);
+          var purpose = piChild.val().purpose;
+          var cost = parseInt(piChild.val().amount);
+
+          if(!payInviters.hasOwnProperty(purpose)){
+            payInviters[purpose] = {};
+          }
+          if(!payInviters[purpose].hasOwnProperty(inviter)){
+            payInviters[purpose][inviter] = {};
+          }
+          if(!payInviters[purpose][inviter].hasOwnProperty(invitee)){
+            payInviters[purpose][inviter][invitee] = 1;
+          }
+          else{payInviters[purpose][inviter][invitee]++}
+
+          purpList[purpose] = (purpList.hasOwnProperty(purpose) ? {"cost":purpList[purpose]["cost"]+cost,"count":purpList[purpose]["count"] + 1} :{"cost":cost,"count":1});
+        });
+
+        //find most common purpse
+        for(key in purpList){
+          if( purpList[key]["count"] > maxPurpNum ){
+            maxPurpNum = purpList[key]["count"];
+            maxPurpose = key;
+          }
+        }
+
+        //find most expensive purpose (avg payment)
+        var expPurpNum = 0;
+        var expPurpose = "";
+        for(key in purpList){
+          if( purpList[key]["cost"]/purpList[key]["count"] > expPurpNum ){
+            expPurpNum = purpList[key]["cost"]/purpList[key]["count"];
+            expPurpose = key;
+          }
+        }
+
+        //calculate average invitees for a common payment (same invitee and purpose)
+        for(purp in payInviters){
+          var invitees = 0;
+          var inviters = 0;
+          for(inv in payInviters[purp]){
+            inviters++;
+            for(invitee in payInviters[purp][inv]){
+              invitees++;
+            }
+          }
+          purpList[purp]["avg"] = (inviters == 0 ? 0 : invitees/inviters);
+        }
+
+        //create FlexList for payment purpose data
+        var purpFlexList = '<div class="flex-list" id="purposeList">Top Purposes for Payment Invites</div>';
+        var count = 0;
+        var array = [];
+
+        for(key in purpList){
+          array.push([key,purpList[key]["count"],purpList[key]["cost"],purpList[key]["avg"]]);
+        }
+        array.sort(function(a,b){return b[1]-a[1]}); //sort by most invites sent
+
+        for(var i = 0; i < array.length; i++){
+          var ari = array[i];
+          purpFlexList += '<div class="flex-list" id="' + ari[0] + '">' + (++count) + '. ' + ari[0] + ': ' + ari[1] + ' &nbsp&nbsp Avg Cost: ' + ari[2]/ari[1] + ' &nbsp&nbsp Avg Inv: '+ ari[3] + '</div>';
+          if(count >= 20){
+            break;
+          }
+        }
+        while(count < 20){
+          count++;
+          purpFlexList += '<div class="flex-list" id="empty"></div>';
+        }
+        purpFlexList += '<div class="flex-list" id="tail" style="background:rgba(5,5,5,0);font-size:0px;margin-top:0px;padding-top:0px"><font color = orange>.</div>';
+
+        var numInviters = {};
+        firebase.database().ref().child("invitedNumbers").once("value", function(inSnap){
+          inSnap.forEach(function(number){
+            var inviter = number.val()["inviter"];
+            if(!numInviters.hasOwnProperty(inviter)){
+              numInviters[inviter] = {
+                "joined" : 0,
+                "total" : 0,
+              };
+            }
+            if(number.val()["joined"]){
+              numInviters[inviter]["joined"]++;
+            }
+            numInviters[inviter]["total"]++;
+          });
+
+          //make FlexList for number invite data
+          var recipFlexList = '<div class="flex-list" id="purposeList">Most Number Invites</div>';
+          var count = 0;
+          var array = [];
+
+          for(key in numInviters){
+            array.push([key,numInviters[key]["total"],numInviters[key]["joined"]]);
+          }
+          array.sort(function(a,b){return b[1]-a[1]}); //sort by most number invites sent
+          for(var i = 0; i < array.length; i++){
+            var ari = array[i];
+            recipFlexList += '<div class="flex-list" id="' + ari[0] + '">' + (++count) + '. ' + ari[0] + ': ' + ari[1] + ' &nbsp&nbsp Acc: ' + Math.round((ari[1] == 0 ? 0 : ari[2]/ari[1])*100) + '%</div>';
+            if(count >= 20){
+              break;
+            }
+          }
+
+          while(count < 20){
+            count++;
+            recipFlexList += '<div class="flex-list" id="empty"></div>';
+          }
+          recipFlexList += '<div class="flex-list" id="tail" style="background:rgba(5,5,5,0);font-size:0px;margin-top:0px;padding-top:0px"><font color = orange>.</div>';
+
+
+
+          document.getElementById("invPurpose").innerHTML = "<br/>Most Common Pay Inv:<br/>" + maxPurpose + "<br/>Total: " + maxPurpNum + "<br/><br/>Priciest Pay Inv:<br/>" + expPurpose + "<br/>Avg Cost: " + expPurpNum;
+          document.getElementById("invRecip").innerHTML = "<br/><br/>Invitee is Recip:<br/>" + (sender == -recip ? 0 : Math.round(100*recip/(sender+recip))) + " %<br/><br/>Total PayInvites: <font size = 3>" + (sender+recip);
+          document.getElementById("invPurpose").onclick = function(){document.getElementById("display").innerHTML = purpFlexList};
+          document.getElementById("invRecip").onclick = function(){document.getElementById("display").innerHTML = recipFlexList};
+        });
+      });
     });
   });//end of snapshot
 });//end of main
+
 
 //Recursively gets the max and average chain length, avg ppl per invite, num chains
 function chainData(rev, uid, chain){
--- userMetrics.js
+++ userMetrics.js
@@ -33,24 +33,27 @@ $(document).ready(function(){
       data["Average Duration"] = avg;
       durs[flowName] = data;
 
-      if( avg > maxAvg ){
+      if( avg > maxAvg ){//check max
         maxAvg = avg;
         slowestFlow = cSnapshot.key;
       }
-      else if( avg < minAvg ){
+      else if( avg < minAvg ){//check min
         minAvg = avg;
         fastestFlow = cSnapshot.key;
       }
 
     }); //end of cSnapshot
 
+
+    //make FlexList for flow data
     var flowFlexList = '<div class="flex-list" id="purposeList">Fastest Flows (Average)</div>';
     var count = 0;
     var array = [];
+
     for(key in durs){
       array.push([key,durs[key]["Average Duration"]]);
     }
-    array.sort(function(a,b){return a[1]-b[1]}); //sort by most invites sent
+    array.sort(function(a,b){return a[1]-b[1]}); //sort by smallest flow time
 
     for(var i = 0; i < array.length; i++){
       flowFlexList += '<div class="flex-list" id="' + array[i][0] + '">' + (++count) + '. ' + array[i][0] + ': ' + Math.round(array[i][1]*10)/10 + ' seconds</div>';
@@ -68,11 +71,12 @@ $(document).ready(function(){
     document.getElementById("flowSpeeds").onclick = function(){document.getElementById("display").innerHTML = flowFlexList};
   });
 
+
+  //make button reference
   var buttRef = umRef.child("buttonPress");
   //init storage
-  var buttPushers = {};
-  var data = {};
-  var text =
+  var buttPushers = {};//store user data for number of times pressing each button
+  var data = {};//store button data for max/avg times a user presses the button
   buttRef.once("value", function(snapshot){ //snapshot -> buttonPress
     snapshot.forEach(function(cSnapshot){ // cSnap -> buttons
       var butt = cSnapshot.key
@@ -91,7 +95,7 @@ $(document).ready(function(){
           buttPushers[user] = {};
         }
         var count = 0;
-        gcSnapshot.forEach(function(ggcSnapshot){// gcSnap -> instances of pressing
+        gcSnapshot.forEach(function(ggcSnapshot){// ggcSnap -> instances of pressing
           count++;
         });
         max = (count > max ? count : max);
@@ -101,7 +105,28 @@ $(document).ready(function(){
       data[butt]["Max"] = max;
       data[butt]["Avg"] = (num = 0 ? 0 : tot/num);
     });
+
+    var buttFlexList = '<div class="flex-list" id="purposeList">Most Pressed Buttons (Average)</div>';
+    var count = 0;
+    var array = [];
+    for(key in data){
+      array.push([key,data[key]["Avg"],data[key]["Max"]]);
+    }
+    array.sort(function(a,b){return b[1]-a[1]}); //sort by most invites sent
+
+    for(var i = 0; i < array.length; i++){
+      buttFlexList += '<div class="flex-list" id="' + array[i][0] + '">' + (++count) + '. ' + array[i][0] + ': ' + Math.round(array[i][1]*100)/100 + ' presses &nbsp&nbsp&nbsp Max: ' + array[i][2] + '</div>';
+      if(count >= 20){
+        break;
+      }
+    }
+    while(count < 20){
+      count++;
+      buttFlexList += '<div class="flex-list" id="empty"></div>';
+    }
+    buttFlexList += '<div class="flex-list" id="tail" style="background:rgba(5,5,5,0);font-size:0px;margin-top:0px;padding-top:0px"><font color = orange>.</div>';
+
     document.getElementById("buttStats").innerHTML = "<br/>User Button Presses<br/><br/>newPayment<br/>Max: " + data["newPayment"]["Max"] + "   Avg: " + Math.round(data["newPayment"]["Avg"]*100)/100 + "<br/><br/>trendingPayments<br/>Max: " + data["trendingPayments"]["Max"] + "   Avg: " + Math.round(data["trendingPayments"]["Avg"]*100)/100;
-    document.getElementById("buttStats").onclick = function(){document.getElementById("display").innerHTML = JSONtolist(data)}
+    document.getElementById("buttStats").onclick = function(){document.getElementById("display").innerHTML = buttFlexList};
   });
 });
